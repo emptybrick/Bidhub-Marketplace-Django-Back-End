ba2f021e3e34bd08d3ed2415f272aaebdb70839f
Emptybrick <im.emptybrick@gmail.com>
2025-10-21 21:12:14 -0400

    fixed backend bids submission, can now submit bids

--- bids/views.py
+++ bids/views.py
@@ -1,3 +1,4 @@
+import decimal
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -18,33 +19,39 @@ from items.serializers.common import ItemSerializer
 class CreateBid(APIView):
     def post(self, request, item_id):
         """Create a new bid for an item"""
+        bid_str = request.data["bid"]
+
         try:
             # Check if item exists
             item = Item.objects.get(pk=item_id)
-
+            # assign request data to variable
+            bid = decimal.Decimal(bid_str)
             if item.owner.id == request.user.id:
                 return Response({"detail": "Cannot bid on your own item"}, status=status.HTTP_400_BAD_REQUEST)
-
             # Check if auction has ended
             if item.end_time and item.end_time < timezone.now():
                 return Response({"detail": "Auction has ended"}, status=status.HTTP_400_BAD_REQUEST)
+            # prevent negative bids and bids of 0
+            if bid <= 0:
+                return Response({"detail": "Bid must be greater than zero"}, status=status.HTTP_400_BAD_REQUEST)
 
             # Get highest bid for this item and compare to bid offer
             current_bid = item.current_bid
-            if current_bid >= request.data["bid"]:
+            if current_bid >= bid:
+
                 return Response({"detail": "Bid offer is lower than or equal to current bid"}, status=status.HTTP_400_BAD_REQUEST)
 
             # Set the bidder to current user and item to the specified item
             bid_data = request.data.copy()
             bid_data['user_id'] = request.user.id
-            bid_data['item_id'] = item_id
+            bid_data['item_id'] = item.id
             item.highest_bidder = request.user
             # Create serializer with the data
             bid_to_create = BidSerializer(data=bid_data)
 
             if not bid_to_create.is_valid(raise_exception=True):
                 return Response(BidSerializer.errors, status=status.HTTP_400_BAD_REQUEST)
-            
+
             bid_to_create.save()
 
             item_data = {
