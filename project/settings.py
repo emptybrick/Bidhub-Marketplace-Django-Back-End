074a1b306838c58661908134d0e445ece8e6dd77
Emptybrick <im.emptybrick@gmail.com>
2025-10-27 14:45:40 -0400

    revert pre2fa to fix login and admin portal issues

--- project/settings.py
+++ project/settings.py
@@ -47,9 +47,11 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:5173",
+# ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOW_CREDENTIALS = True
 
